014937dcb13a66c69c3ae6c7e6f706d30c2abd3d
Bebaz <limaafonso57@gmail.com>
2024-05-11 23:50:24 +0100

    Java Scrpit added and page is now identical. Almost in the end :)

---
+++ Untitled-.js
@@ -0,0 +1,24 @@
+
+let errorMsg = document.getElementById("error")
+let emailform = document.getElementById("form");
+
+// Add event listener for form submission
+emailform.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let email = document.getElementById("email");
+
+    let emailValue = email.value;
+
+    let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (validRegex.test(emailValue)) {
+        console.log("Email is valid:", emailValue);
+
+    } else {
+        console.error("Invalid email:", emailValue);
+        errorMsg.textContent = "Valid email required"
+        email.style.border = "1px solid red";
+        email.style.color = "red"
+    }
+});
